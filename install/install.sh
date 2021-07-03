fc2db2c825daf0936543d08c68f921eaf46d1aae
Josiah Bull <josiah.bull7@gmail.com>
2021-07-03 15:51:33 +1200

    feat (intall): Wrote a basic installer to setup the program on new machines.

---
+++ install/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Small install script to install the management script more conviently.
+
+# Check we are sudo https://stackoverflow.com/questions/18215973/how-to-check-if-running-as-root-in-a-bash-script
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# Copy the binary to the correct location
+cp ../target/release/vm-restart-script /bin/vm-restart-script
+
+# Create the service script
+cp ./virtual-machine-manager.service /etc/systemd/system/virtual-machine-manager.service
+
+systemctl enable virtual-machine-manager.service
+systemctl start virtual-machine-manager.service
